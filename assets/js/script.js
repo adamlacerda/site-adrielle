aad79ef1128dbba52a53ab3f61769c86b14b77fa
Eduardo Diniz <diniz480@gmail.com>
2023-04-03 21:44:21 -0300

    Modificação do nav e construção pag sobre

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+function openNav() {
+    document.getElementById("menu-nav").style.height = "100%";
+    document.querySelector("body").style.overflow = "hidden";
+  }
+
+  function closeNav() {
+    document.getElementById("menu-nav").style.height = "0%";
+    document.querySelector("body").style.overflow = "auto";
+  }
+
+ 
